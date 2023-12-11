a4847481503e7b153101bd1c2b2583afce4b68a4
Andrii <andpris527@gmail.com>
2023-12-11 17:19:05 +0200

    Add squares and animation

---
+++ square.js
@@ -0,0 +1,61 @@
+class Square {
+	#x = 0;
+	#y = 0;
+	#size = 0;
+	#color = 0;
+	#speedX = 0;
+	#speedY = 0;
+	#canvas = 0;
+	#context = 0;
+
+	constructor(x, y, size, color, speedX, speedY, canvas) {
+		this.#x = x;
+		this.#y = y;
+		this.#size = size;
+		this.#color = color;
+		this.#speedX = speedX;
+		this.#speedY = speedY;
+		this.#canvas = canvas;
+		this.#context = this.#canvas.getContext("2d");
+
+		this.draw();
+	}
+
+	draw() {
+		this.#context.fillStyle = this.#color;
+		this.#context.fillRect(this.#x, this.#y, this.#size, this.#size);
+	}
+
+	move() {
+		this.#x += this.#speedX;
+		this.#y += this.#speedY;
+	}
+
+	checkCollisions() {
+		if(this.#x < 0 || (this.#x+this.#size) > this.#canvas.width)
+			this.#speedX = - this.#speedX;
+		if(this.#y < 0 || (this.#y+this.#size) > this.#canvas.height)
+			this.#speedY = - this.#speedY;
+	}
+}
+
+let canvas = document.getElementById("anim");
+let context = canvas.getContext("2d");
+
+let square1Size = 10;
+let square2Size = 20;
+
+let square1 = new Square(0, (canvas.height-square1Size)/2, square1Size, "red", 3, 0, canvas);
+let square2 = new Square((canvas.width-square2Size)/2, 0, square2Size, "green", 0, 2, canvas);
+
+setInterval(() => {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	square1.draw();
+	square1.move();
+	square1.checkCollisions();
+
+	square2.draw();
+	square2.move();
+	square2.checkCollisions();
+}, 30);
