cfbc6f69bea7dc0831a98d8bb13d09070b0593ce
Andrii <andpris527@gmail.com>
2023-12-11 19:28:01 +0200

    Add start button

--- work_creator.js
+++ work_creator.js
@@ -4,11 +4,17 @@ async function createWorkArea() {
 
 	let controls = document.createElement('section');
 	controls.classList.add('controls');
-	
+
 	let closeBtn = document.createElement('button');
 	closeBtn.textContent = "Close";
 	closeBtn.addEventListener("click", removeWorkArea);
 	controls.append(closeBtn);
+
+	let startBtn = document.createElement('button');
+	startBtn.textContent = "Start";
+	startBtn.setAttribute('id', 'startBtn');
+	startBtn.addEventListener("click", startButtonClick);
+	controls.append(startBtn);
 
 	let anim = document.createElement('canvas');
 	anim.classList.add('anim');
@@ -32,6 +38,9 @@ function removeWorkArea() {
 async function start() {
 	let promise = createWorkArea();
 	let result = await promise;
+}
 
+function startButtonClick(event) {
+	event.target.style.display = "none";
 	startAnimation();
 }
